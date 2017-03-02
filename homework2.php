fbb4dc65fbde56c7d0f0f45277699ba624623f79
HukkeJlb <hukkejlb@gmail.com>
2017-03-02 13:41:03 +0300

    homework2_(1-2 completed)fixed

--- homework2.php
+++ homework2.php
@@ -6,76 +6,90 @@ $array1 = [
     2 => "Digital Chaos, Evil Geniuses, Team NP",
     3 => "Newbee, Wings, VG.J"
 ];
-function cool_func($array, $case = null) {
-    if ($case == true) {
+function cool_func($array, $case = false) {
+    if ($case) {
         $resFunction = implode(', ', $array);
         return $resFunction;
     } else {
-        foreach ($array as $key => $value) {
+        foreach ($array as $value) {
             echo "<p>" . $value . "</p>";
-        };
-    };
-};
+        }
+    }
+}
+
 cool_func($array1);
 echo "<br>===========================================================================<br>";
 $asd = cool_func($array1, true);
 echo "$asd";
 //===============================================================================
 echo "<h2>EXERCISE №2<br>_________________________<br></h2>";
-operation([3,2,2], '**');
+operation([3, 2, 0], '/');
 function operation($data, $operation) {
-    if (is_array($data) == false) {
+    if (!is_array($data)) {
         echo 'Необходимо ввести массив чисел<br>';
         return;
-    };
-    for ($i = 0; $i < count($data); $i++) {
-        if ((is_int($data[$i]) == false)) {
+    }
+    foreach ($data as $value) {
+        if (!is_int($value)) {
             echo 'Некоректный массив! Введите только числа!<br>';
             return;
-        };
-    };
-    if ($operation == "+" || $operation == '-' || $operation == '/' || $operation == '*' || $operation == '**') {
-        switch ($operation) {
-            case "+" :
-                $rez = 0;
-                for ($i = 0; $i <= count($data); $i++) {
-                    $rez = $rez+$data[$i];
-                };
-                echo "Результат вычислений = $rez";
-                break;
-            case "-" :
-                $rez = $data[0];
-                for ($i = 0; $i <= count($data); $i++) {
-                    $rez = $rez-$data[$i+1];
-                };
-                echo "Результат вычислений = $rez";
-                break;
-            case "*" :
-                $rez = 1;
-                for ($i = 0; $i < count($data); $i++) {
-                    $rez = $rez*$data[$i];
-                };
-                echo "Результат вычислений = $rez";
-                break;
-            case "/" :
-                $rez = $data[0];
-                for ($i = 1; $i < count($data); $i++) {
-                    $rez = $rez/$data[$i];
-                };
-                echo "Результат вычислений = $rez";
-                break;
-            case "**" :
-                $rez = $data[0];
-                for ($i = 0; $i < (count($data)-1); $i++) {
-                    $rez = pow($rez, $data[$i+1]);
-                };
-                echo "Результат вычислений = $rez";
-                break;
         }
-    } else {
-        echo "Некорректный арифметический оператор";
     }
-};
+    switch ($operation) {
+        case "+" :
+            $rez = 0;
+            for ($i = 0; $i <= count($data); $i++) {
+                $rez = $rez + $data[$i];
+            }
+            echo "Результат вычислений = $rez";
+            break;
+        case "-" :
+            $rez = $data[0];
+            for ($i = 0; $i <= count($data); $i++) {
+                $rez = $rez - $data[$i + 1];
+            }
+            echo "Результат вычислений = $rez";
+            break;
+        case "*" :
+            $rez = 1;
+            for ($i = 0; $i < count($data); $i++) {
+                $rez = $rez * $data[$i];
+            }
+            echo "Результат вычислений = $rez";
+            break;
+        case "/" :
+            $rez = $data[0];
+            for ($i = 1; $i < count($data); $i++) {
+                $rez = $rez / $data[$i];
+                if ($data[$i] == 0) {
+                    echo "Деление на ноль - запрещено<br>";
+                    return;
+                }
+            }
+            echo "Результат вычислений = $rez";
+            break;
+        case "**" :
+            $rez = $data[0];
+            for ($i = 0; $i < (count($data) - 1); $i++) {
+                $rez = pow($rez, $data[$i + 1]);
+            }
+            echo "Результат вычислений = $rez";
+            break;
+        default:
+            echo "Некорректный арифметический оператор";
+    }
+}
 
-
-
+//===============================================================================
+echo "<h2>EXERCISE №3<br>_________________________<br></h2>";
+//function calc($data, $operation) {
+//    if (is_array($data)) {
+//        echo 'Необходимо ввести массив чисел<br>';
+//        return;
+//    }
+//    foreach ($data as $value) {
+//        if ((is_int($value))) {
+//            echo 'Некоректный массив! Введите только числа!<br>';
+//            return;
+//        }
+//    }
