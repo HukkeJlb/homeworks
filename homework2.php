48104a5d4b345ebf94bf8a91e36e8d4358d0695b
HukkeJlb <hukkejlb@gmail.com>
2017-03-02 15:31:35 +0300

    homework2_(1-3 completed)

--- homework2.php
+++ homework2.php
@@ -6,7 +6,8 @@ $array1 = [
     2 => "Digital Chaos, Evil Geniuses, Team NP",
     3 => "Newbee, Wings, VG.J"
 ];
-function cool_func($array, $case = false) {
+function cool_func($array, $case = false)
+{
     if ($case) {
         $resFunction = implode(', ', $array);
         return $resFunction;
@@ -15,6 +16,7 @@ function cool_func($array, $case = false) {
             echo "<p>" . $value . "</p>";
         }
     }
+    return null;
 }
 
 cool_func($array1);
@@ -24,7 +26,8 @@ echo "$asd";
 //===============================================================================
 echo "<h2>EXERCISE №2<br>_________________________<br></h2>";
 operation([3, 2, 0], '/');
-function operation($data, $operation) {
+function operation($data, $operation)
+{
     if (!is_array($data)) {
         echo 'Необходимо ввести массив чисел<br>';
         return;
@@ -82,14 +85,108 @@ function operation($data, $operation) {
 
 //===============================================================================
 echo "<h2>EXERCISE №3<br>_________________________<br></h2>";
-//function calc($data, $operation) {
-//    if (is_array($data)) {
-//        echo 'Необходимо ввести массив чисел<br>';
-//        return;
-//    }
-//    foreach ($data as $value) {
-//        if ((is_int($value))) {
-//            echo 'Некоректный массив! Введите только числа!<br>';
-//            return;
-//        }
-//    }
+calcEverything('*', 5, 3, 1.5);
+function calcEverything()
+{
+    $argArray = func_get_args();
+    $arr = array_splice($argArray, 1);
+    foreach ($arr as $value) {
+        if ((!is_int($value)) && (!is_float($value))) {
+            echo 'Введите корректные аргументы(2-ой и последующие аргумент должны быть числами)';
+            return;
+        }
+    }
+    switch ($argArray[0]) {
+        case '+':
+            $rez = 0;
+            for ($i = 0; $i <= count($arr); $i++) {
+                $rez = $rez + $arr[$i];
+            }
+            $output = '';
+            for ($a = 0; $a < count($arr); $a++){
+                $q = "$arr[$a]";
+                $w = "$argArray[0]";
+                if ($a == (count($arr)-1)){
+                    $output =$output.$q;
+                } else {
+                    $output = $output.$q.$w;
+                }
+            }
+            echo "$output=$rez";
+            break;
+        case '-':
+            $rez = $arr[0];
+            for ($i = 0; $i <= count($arr); $i++) {
+                $rez = $rez - $arr[$i + 1];
+            }
+            $output = '';
+            for ($a = 0; $a < count($arr); $a++){
+                $q = "$arr[$a]";
+                $w = "$argArray[0]";
+                if ($a == (count($arr)-1)){
+                    $output =$output.$q;
+                } else {
+                    $output = $output.$q.$w;
+                }
+            }
+            echo "$output=$rez";
+            break;
+        case '/':
+            $rez = $arr[0];
+            for ($i = 1; $i < count($arr); $i++) {
+                $rez = $rez / $arr[$i];
+                if ($arr[$i] == 0) {
+                    echo "Деление на ноль - запрещено<br>";
+                    return;
+                }
+            }
+            $output = '';
+            for ($a = 0; $a < count($arr); $a++){
+                $q = "$arr[$a]";
+                $w = "$argArray[0]";
+                if ($a == (count($arr)-1)){
+                    $output =$output.$q;
+                } else {
+                    $output = $output.$q.$w;
+                }
+            }
+            echo "$output=$rez";
+            break;
+        case '*':
+            $rez = 1;
+            for ($i = 0; $i < count($arr); $i++) {
+                $rez = $rez * $arr[$i];
+            }
+            $output = '';
+            for ($a = 0; $a < count($arr); $a++){
+                $q = "$arr[$a]";
+                $w = "$argArray[0]";
+                if ($a == (count($arr)-1)){
+                    $output =$output.$q;
+                } else {
+                    $output = $output.$q.$w;
+                }
+            }
+            echo "$output=$rez";
+            break;
+        case '**':
+            $rez = $arr[0];
+            for ($i = 0; $i < (count($arr) - 1); $i++) {
+                $rez = pow($rez, $arr[$i + 1]);
+            }
+            $output = '';
+            for ($a = 0; $a < count($arr); $a++){
+                $q = "$arr[$a]";
+                $w = "$argArray[0]";
+                if ($a == (count($arr)-1)){
+                    $output =$output.$q;
+                } else {
+                    $output = $output.$q.$w;
+                }
+            }
+            echo "$output=$rez";
+            break;
+        default:
+            echo 'Первый аргумент должен содержать корректный арифметический оператор<br>';
+    }
+}
