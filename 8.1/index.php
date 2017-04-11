8fbeb949c3b96aab0348c03012218fc770b3bea1
HukkeJlb <hukkejlb@gmail.com>
2017-04-11 04:15:47 +0300

    homework 8.1 - готово для проверки

--- 8.1/index.php
+++ 8.1/index.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>ДЗ №8.1</title>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/bootstrap-theme.css"/>
 </head>
 <body>
 <h1>Категории и их товары</h1>
-<button type="submit" id="submit">show</button>
-<div id="table"></div>
+<button id="submit">show</button>
+<div class="container" id="goods">
+</div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="main.js"></script>
--- 8.1/main.js
+++ 8.1/main.js
@@ -1,15 +1,23 @@
 var button = $('#submit');
 button.on('click', function () {
-    var title = $('input[name=title]').val();
 
     $.ajax({
         url: 'rest.php',
         method: 'GET',
-        data: {
-            title: title
-        },
         dataType: 'json'
-    }).done(function (data) {
-        console.log(data);
+    }).done(function (table){
+        goods = '<table class="table table-bordered"><tr><th>Категория</th><th>Артикул</th><th>Название</th><th>Бренд</th><th>Описание</th><th>Цена</th><th>Скидка</th><th>Количество покупок</th></tr>';
+        for (var key = 0; key < table.length; key++) {
+            goods +='<td>'+table[key].category_id+'</td>';
+            goods +='<td>'+table[key].article+'</td>';
+            goods +='<td>'+table[key].name+'</td>';
+            goods +='<td>'+table[key].brand+'</td>';
+            goods +='<td>'+table[key].description+'</td>';
+            goods +='<td>'+table[key].price+'</td>';
+            goods +='<td>'+table[key].discount+'</td>';
+            goods +='<td>'+table[key].purchased+'</td></tr>';
+        }
+        goods += '</table>';
+        $('#goods').html(goods);
     });
 })
